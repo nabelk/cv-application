ed2f2486a809e4775cecd8860babc7cdd0d2704c
Nabil Khalid <holanabilkhalid@gmail.com>
2024-01-14 03:27:58 +0800

    chore: clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { GeneralInfoForm } from './components/general-form';
 import { EducationForm } from './components/education-form';
 import { WorkForm } from './components/work-form';
-import generalInfo from './assets/generalInfo.json';
-import education from './assets/education.json';
 
 function App() {
     return (
         <>
-            {/*   <Section title='General Information' listInput={generalInfo}></Section>
-            <Section title='Education' listInput={education}></Section> */}
             <GeneralInfoForm></GeneralInfoForm>
             <EducationForm></EducationForm>
             <WorkForm></WorkForm>
