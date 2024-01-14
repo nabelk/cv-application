975a86224bc622f56bf1fa20a802f04bd29e60b2
Nabil Khalid <holanabilkhalid@gmail.com>
2024-01-15 05:11:31 +0800

    update: add handle input change & refactor structure of the component

--- src/components/general-form.jsx
+++ src/components/general-form.jsx
@@ -1,17 +1,64 @@
+import { useState } from 'react';
 import { Input } from './input';
-import generalInfo from '../assets/generalInfo.json';
 
-export function GeneralInfoForm() {
-    const listItems = generalInfo.map((info) => (
-        <li key={info.id}>
-            <Input label={info.label} placeholder={info.placeholder} />
-        </li>
+export function GeneralInfoForm({ initialGeneralData, onFormDataChange }) {
+    const [personalInfo, setPersonalInfo] = useState(initialGeneralData);
+
+    const handleInputChange = (e, field) => {
+        const newPersonalInfo = [{ ...personalInfo[0], [field]: e.target.value }];
+        setPersonalInfo(newPersonalInfo);
+        onFormDataChange(newPersonalInfo, 0);
+    };
+
+    const listItems = personalInfo.map((info, idx) => (
+        <ul key={idx} style={{ listStyle: 'none' }}>
+            <li>
+                <Input
+                    label='First Name'
+                    placeholder='Enter first name'
+                    value={info.firstName}
+                    onChange={(e) => handleInputChange(e, 'firstName')}
+                ></Input>
+            </li>
+            <li>
+                <Input
+                    label='Last Name'
+                    placeholder='Enter last name'
+                    value={info.lastName}
+                    onChange={(e) => handleInputChange(e, 'lastName')}
+                ></Input>
+            </li>
+            <li>
+                <Input
+                    label='Email'
+                    placeholder='Enter email'
+                    value={info.email}
+                    onChange={(e) => handleInputChange(e, 'email')}
+                ></Input>
+            </li>
+            <li>
+                <Input
+                    label='Phone number'
+                    placeholder='Enter phone number'
+                    value={info.phoneNumber}
+                    onChange={(e) => handleInputChange(e, 'phoneNumber')}
+                ></Input>
+            </li>
+            <li>
+                <Input
+                    label='Job title'
+                    placeholder='Enter job title'
+                    value={info.jobTitle}
+                    onChange={(e) => handleInputChange(e, 'jobTitle')}
+                ></Input>
+            </li>
+        </ul>
     ));
 
     return (
         <section>
             <h2>General Information</h2>
-            <ul style={{ listStyle: 'none' }}>{listItems}</ul>
+            {listItems}
         </section>
     );
 }
