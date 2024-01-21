b1675409ccfd6ff66707cc3accbfcb50f1a26203
Nabil Khalid <holanabilkhalid@gmail.com>
2024-01-21 22:24:24 +0800

    feat: implement show/hide functionality in the children of listitems

--- src/components/education-form.jsx
+++ src/components/education-form.jsx
@@ -3,6 +3,89 @@ import { Input } from './input';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCaretUp, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
+function ListItem({ list, handleInputChange, handleDeleteEducation, isNewList }) {
+    const [showItem, setShowItem] = useState(isNewList);
+
+    return (
+        <ul style={{ listStyle: 'none' }}>
+            {showItem ? (
+                <>
+                    <div>
+                        <button onClick={() => handleDeleteEducation(list.id)}>
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                        <span onClick={() => setShowItem(false)}>
+                            <FontAwesomeIcon icon={faCaretUp} />
+                        </span>
+                    </div>
+                    <li>
+                        <Input
+                            label='School'
+                            placeholder='Enter School'
+                            value={list.school}
+                            onChange={(e) => {
+                                handleInputChange(e, list.id, 'school');
+                            }}
+                        ></Input>
+                    </li>
+                    <li>
+                        <Input
+                            label='Degree'
+                            placeholder='Enter Degree'
+                            value={list.degree}
+                            onChange={(e) => {
+                                handleInputChange(e, list.id, 'degree');
+                            }}
+                        ></Input>
+                    </li>
+                    <div
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: '1fr 1fr',
+                            gap: '9px',
+                        }}
+                    >
+                        <li>
+                            <Input
+                                label='Start date'
+                                value={list.start_date}
+                                onChange={(date) => {
+                                    handleInputChange(date, list.id, 'start_date');
+                                }}
+                            ></Input>
+                        </li>
+                        <li>
+                            <Input
+                                label='End date'
+                                value={list.end_date}
+                                onChange={(date) => {
+                                    handleInputChange(date, list.id, 'end_date');
+                                }}
+                            ></Input>
+                        </li>
+                    </div>
+                </>
+            ) : (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        width: '100%',
+                        alignItems: 'center',
+                    }}
+                >
+                    <h4 style={{ margin: 0 }}>
+                        {list.degree ? list.degree : `Education ${list.id}`}
+                    </h4>
+                    <span onClick={() => setShowItem(true)}>
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </span>
+                </div>
+            )}
+        </ul>
+    );
+}
+
 export function EducationForm({ initialEducationData, onFormDataChange }) {
     const [educationList, setEducationList] = useState(initialEducationData);
     const [showEdu, setShowEdu] = useState(false);
@@ -39,57 +122,13 @@ export function EducationForm({ initialEducationData, onFormDataChange }) {
     };
 
     const listItems = educationList.map((list) => (
-        <ul key={list.id} style={{ listStyle: 'none' }}>
-            <button onClick={() => handleDeleteEducation(list.id)}>
-                <FontAwesomeIcon icon={faTrashCan} />
-            </button>
-            <li>
-                <Input
-                    label='School'
-                    placeholder='Enter School'
-                    value={list.school}
-                    onChange={(e) => {
-                        handleInputChange(e, list.id, 'school');
-                    }}
-                ></Input>
-            </li>
-            <li>
-                <Input
-                    label='Degree'
-                    placeholder='Enter Degree'
-                    value={list.degree}
-                    onChange={(e) => {
-                        handleInputChange(e, list.id, 'degree');
-                    }}
-                ></Input>
-            </li>
-            <div
-                style={{
-                    display: 'grid',
-                    gridTemplateColumns: '1fr 1fr',
-                    gap: '9px',
-                }}
-            >
-                <li>
-                    <Input
-                        label='Start date'
-                        value={list.start_date}
-                        onChange={(date) => {
-                            handleInputChange(date, list.id, 'start_date');
-                        }}
-                    ></Input>
-                </li>
-                <li>
-                    <Input
-                        label='End date'
-                        value={list.end_date}
-                        onChange={(date) => {
-                            handleInputChange(date, list.id, 'end_date');
-                        }}
-                    ></Input>
-                </li>
-            </div>
-        </ul>
+        <ListItem
+            key={list.id}
+            list={list}
+            handleInputChange={handleInputChange}
+            handleDeleteEducation={handleDeleteEducation}
+            isNewList={!list.degree ?? true}
+        ></ListItem>
     ));
 
     return (
--- src/components/work-form.jsx
+++ src/components/work-form.jsx
@@ -3,6 +3,115 @@ import { Input } from './input';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCaretUp, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
+function ListItem({ list, handleInputChange, handleDeleteExperience, handleKeyDown, isNewList }) {
+    const [showItem, setShowItem] = useState(isNewList);
+
+    return (
+        <ul key={list.id} style={{ listStyle: 'none' }}>
+            {showItem ? (
+                <>
+                    {' '}
+                    <div>
+                        <button onClick={() => handleDeleteExperience(list.id)}>
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                        <span onClick={() => setShowItem(false)}>
+                            <FontAwesomeIcon icon={faCaretUp} />
+                        </span>
+                    </div>
+                    <li>
+                        <Input
+                            label='Title'
+                            placeholder='Enter job title'
+                            value={list.title}
+                            onChange={(e) => {
+                                handleInputChange(e, list.id, 'title');
+                            }}
+                        ></Input>
+                    </li>
+                    <li>
+                        <Input
+                            label='Company Name'
+                            placeholder='Enter company name'
+                            value={list.company_name}
+                            onChange={(e) => {
+                                handleInputChange(e, list.id, 'company_name');
+                            }}
+                        ></Input>
+                    </li>
+                    <li>
+                        <Input
+                            label='Employment Type'
+                            placeholder='Ex: Full-time'
+                            value={list.employment_type}
+                            onChange={(e) => {
+                                handleInputChange(e, list.id, 'employment_type');
+                            }}
+                        ></Input>
+                    </li>
+                    <li>
+                        <label htmlFor='description'>Description</label>
+                        <textarea
+                            name='description'
+                            id='description'
+                            placeholder='Enter description about the job'
+                            cols='30'
+                            rows='10'
+                            value={list.description}
+                            onChange={(e) => {
+                                handleInputChange(e, list.id, 'description');
+                            }}
+                            onKeyDown={(e) => handleKeyDown(e, list.id, 'description')}
+                        ></textarea>
+                    </li>
+                    <div
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: '1fr 1fr',
+                            gap: '9px',
+                        }}
+                    >
+                        <li>
+                            <Input
+                                label='Start date'
+                                value={list.start_date}
+                                onChange={(date) => {
+                                    handleInputChange(date, list.id, 'start_date');
+                                }}
+                            ></Input>
+                        </li>
+                        <li>
+                            <Input
+                                label='End date'
+                                value={list.end_date}
+                                onChange={(date) => {
+                                    handleInputChange(date, list.id, 'end_date');
+                                }}
+                            ></Input>
+                        </li>
+                    </div>
+                </>
+            ) : (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        width: '100%',
+                        alignItems: 'center',
+                    }}
+                >
+                    <h4 style={{ margin: 0 }}>
+                        {list.title ? list.title : `Experience ${list.id}`}
+                    </h4>
+                    <span onClick={() => setShowItem(true)}>
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </span>
+                </div>
+            )}
+        </ul>
+    );
+}
+
 export function WorkForm({ initialWorkData, onFormDataChange }) {
     const [workList, setWorkList] = useState(initialWorkData);
     const [showWork, setShowWork] = useState(false);
@@ -51,82 +160,14 @@ export function WorkForm({ initialWorkData, onFormDataChange }) {
     };
 
     const listItems = workList.map((list) => (
-        <ul key={list.id} style={{ listStyle: 'none' }}>
-            <button onClick={() => handleDeleteExperience(list.id)}>
-                <FontAwesomeIcon icon={faTrashCan} />
-            </button>
-            <li>
-                <Input
-                    label='Title'
-                    placeholder='Enter job title'
-                    value={list.title}
-                    onChange={(e) => {
-                        handleInputChange(e, list.id, 'title');
-                    }}
-                ></Input>
-            </li>
-            <li>
-                <Input
-                    label='Company Name'
-                    placeholder='Enter company name'
-                    value={list.company_name}
-                    onChange={(e) => {
-                        handleInputChange(e, list.id, 'company_name');
-                    }}
-                ></Input>
-            </li>
-            <li>
-                <Input
-                    label='Employment Type'
-                    placeholder='Ex: Full-time'
-                    value={list.employment_type}
-                    onChange={(e) => {
-                        handleInputChange(e, list.id, 'employment_type');
-                    }}
-                ></Input>
-            </li>
-            <li>
-                <label htmlFor='description'>Description</label>
-                <textarea
-                    name='description'
-                    id='description'
-                    placeholder='Enter description about the job'
-                    cols='30'
-                    rows='10'
-                    value={list.description}
-                    onChange={(e) => {
-                        handleInputChange(e, list.id, 'description');
-                    }}
-                    onKeyDown={(e) => handleKeyDown(e, list.id, 'description')}
-                ></textarea>
-            </li>
-            <div
-                style={{
-                    display: 'grid',
-                    gridTemplateColumns: '1fr 1fr',
-                    gap: '9px',
-                }}
-            >
-                <li>
-                    <Input
-                        label='Start date'
-                        value={list.start_date}
-                        onChange={(date) => {
-                            handleInputChange(date, list.id, 'start_date');
-                        }}
-                    ></Input>
-                </li>
-                <li>
-                    <Input
-                        label='End date'
-                        value={list.end_date}
-                        onChange={(date) => {
-                            handleInputChange(date, list.id, 'end_date');
-                        }}
-                    ></Input>
-                </li>
-            </div>
-        </ul>
+        <ListItem
+            key={list.id}
+            list={list}
+            handleInputChange={handleInputChange}
+            handleDeleteExperience={handleDeleteExperience}
+            handleKeyDown={handleKeyDown}
+            isNewList={!list.title ?? true}
+        ></ListItem>
     ));
 
     return (
