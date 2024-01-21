fcff0671bbf6ddd77be43591af4086406f797b37
Nabil Khalid <holanabilkhalid@gmail.com>
2024-01-22 03:02:40 +0800

    refactor: finalize structure of header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,25 @@
 import { PdfDownload } from './pdf-download';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { ReactSVG } from 'react-svg';
 
-export function Header() {
+export function Header({ togglesideBar }) {
     return (
         <header>
-            <h4 style={{ margin: 0 }}>CV Generator</h4>
-            <PdfDownload></PdfDownload>
+            <div>
+                <button onClick={togglesideBar}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <h4 style={{ margin: 0, whiteSpace: 'nowrap' }}>CV Builder</h4>
+            </div>
+            <div>
+                <a href='https://github.com/nabelk'>
+                    <button title='Check my github profile'>
+                        <ReactSVG src='../../public/github-logo.svg'></ReactSVG>
+                    </button>
+                </a>
+                <PdfDownload></PdfDownload>
+            </div>
         </header>
     );
 }
